915d7212e00c64c957132382f16a2361659ee018
mini-chip <75009853+mini-chip@users.noreply.github.com>
2025-04-26 19:44:17 +0900

    navbar hover했을 때 sidebar 나타나도록 구현 (#4)
    * Update PULL_REQUEST_TEMPLATE.md
    
    * feat: navbar & logout & 유저정보 구현 완료
    
    * feat: sidebar css
    
    * feat: navbar 기능 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/app/component/navbar/page";
+import "./globals.css";
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <div className="w-20 h-20 bg-black">test</div>
       <h1 className="font-semibold">Welcome to WED</h1>
     </div>
