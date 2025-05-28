e504077fe68c16c765140b768628097fb91fc203
mini-chip <75009853+mini-chip@users.noreply.github.com>
2025-05-28 23:40:50 +0900

    상품 api 연동하기 (#7)
    * feat: api 수정
    
    * feat:fakestoreapi연동 및 필요없는 파일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/app/component/navbar/page";
 import "./globals.css";
+import ProductList from "./productlist/page";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <ProductList />
       <div className="w-20 h-20 bg-black">test</div>
       <h1 className="font-semibold">Welcome to WED</h1>
     </div>
